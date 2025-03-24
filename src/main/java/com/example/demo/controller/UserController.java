132a704219aba983658185e19e0cfd56da80b674
Maksim <masyanya.maksimka@mail.ru>
2025-03-24 11:36:58 +0300

    Создать crud для таблиц

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,46 +1,49 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.UserDTO;
+import com.example.demo.dto.UserRegistrationRequestDto;
+import com.example.demo.dto.UserRegistrationResponseDto;
 import com.example.demo.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    @PostMapping("/registration")
+    public ResponseEntity<UserRegistrationResponseDto> registrationUser(@RequestBody UserRegistrationRequestDto userRegistrationRequestDto){
 
+        return ResponseEntity.ok(userService.saveUser(userRegistrationRequestDto));
+    }
     @GetMapping("/")
-    public ResponseEntity<List<UserDTO>> getUsers(){
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<List<UserRegistrationResponseDto>> getAllUsers(){
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable Integer userId){
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<UserRegistrationResponseDto> getUserById(@PathVariable Integer userId){
         return ResponseEntity.ok(userService.getUserById(userId));
     }
 
+    @PostMapping("/{userId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<UserRegistrationResponseDto> changeUser(@PathVariable Integer userId,@RequestBody UserRegistrationRequestDto userRegistrationRequestDto){
+        return ResponseEntity.ok(userService.changeUser(userId, userRegistrationRequestDto));
+    }
+
+
+
     @DeleteMapping("/{userId}")
-    public ResponseEntity<UserDTO> deleteUser(@PathVariable Integer userId){
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<UserRegistrationResponseDto> deleteUser(@PathVariable Integer userId){
         userService.deleteUser(userId);
         return ResponseEntity.noContent().build();
-    }
-
-    @PostMapping("/")
-    public ResponseEntity<UserDTO> addUser(@Valid @RequestBody UserDTO userDTO){
-//        clientDTO.setStatus(Status.valueOf("ACTIVE"));
-        return ResponseEntity.ok(userService.saveUser(userDTO));
-    }
-
-    @PostMapping("/{userId}")
-    public ResponseEntity<UserDTO> changeUser(@PathVariable Integer userId,
-                                                  @RequestBody UserDTO userDTO){
-        return ResponseEntity.ok(userService.changeUser(userId, userDTO));
     }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.UserDTO;
+
+import com.example.demo.dto.UserRegistrationRequestDto;
+import com.example.demo.dto.UserRegistrationResponseDto;
 import com.example.demo.entity.User;
 import com.example.demo.exceptions.NoSuchException;
-import com.example.demo.mapper.UserMapper;
+import com.example.demo.mapper.UserRegisterRequestMapper;
+import com.example.demo.mapper.UserRegisterResponseMapper;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,72 +23,58 @@ import java.util.stream.Collectors;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final UserRegisterRequestMapper userRegisterRequestMapper;
+    private final UserRegisterResponseMapper userRegisterResponseMapper;
 
     @Transactional
-    public List<UserDTO> getAllUsers() {
+    public List<UserRegistrationResponseDto> getAllUsers() {
         log.info("Get all Users");
-        if (userRepository.findAll().isEmpty()) {
+        if(userRepository.findAll().isEmpty()){
             throw new NoSuchException("No users");
         }
-        return userRepository.findAll().stream().map(userMapper::toDto).collect(Collectors.toList());
+        return userRepository.findAll().stream().map(userRegisterResponseMapper :: toRegistrationResponseDto).collect(Collectors.toList());
     }
 
     @Transactional
-    public UserDTO getUserById(Integer userId) {
-        log.info("Get client by id: {} ", userId);
+    public UserRegistrationResponseDto getUserById(Integer userId) {
+        log.info("Get user by id: {} ", userId);
         Optional<User> userOptional = Optional.ofNullable(userRepository.findById(userId)
-                .orElseThrow(() -> new NoSuchException("There is no user with ID = " + userId + " in DB")));
-        return userMapper.toDto(userOptional.get());
+                .orElseThrow(() -> new NoSuchException("There is no user with ID = "+ userId + " in Database")));
+        return userRegisterResponseMapper.toRegistrationResponseDto(userOptional.get());
     }
 
+    @Transactional
+    public UserRegistrationResponseDto saveUser(UserRegistrationRequestDto userRegistrationRequestDto) {
+        log.info("Saving user: {}", userRegistrationRequestDto);
+        User saveUser = userRepository.save(userRegisterRequestMapper.toEntity(userRegistrationRequestDto));
+        return userRegisterResponseMapper.toRegistrationResponseDto(saveUser);
 
-//    @Transactional
-//    public User registerCli(User user) {
-//        // Проверка, существует ли уже пользователь с таким email
-//        if (clientRepository.findByEmail(client.getEmail()).isPresent()) {
-//            throw new NoSuchException("User with email " + client.getEmail() + " already exists.");
-//        }
-//
-//        // Хеширование пароля
-//        client.setPassword(passwordEncoder.encode(client.getPassword()));
-//
-//        // Сохранение пользователя
-//        return clientRepository.save(client);
-//    }
+    }
+
+    @Transactional
+    public UserRegistrationResponseDto changeUser(Integer userId,UserRegistrationRequestDto userRegistrationRequestDto){
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if(optionalUser.isEmpty()){
+            throw new NoSuchException("There is no user with ID = "+ userId + " in Database");
+        }else{
+            User existingUser = optionalUser.get();
+            User updatedUser = userRegisterRequestMapper.toEntity(userRegistrationRequestDto);
+            existingUser.setEmail(updatedUser.getEmail());
+            existingUser.setPassword(updatedUser.getPassword());
+            existingUser.setName(updatedUser.getName());
+            existingUser.setLastname(updatedUser.getLastname());
+            existingUser.setRole(updatedUser.getRole());
+
+            return userRegisterResponseMapper.toRegistrationResponseDto(userRepository.save(existingUser));
+        }
+    }
 
     @Transactional
     public void deleteUser(Integer userId) {
         log.info("Delete user");
-        if (userRepository.findById(userId).isEmpty()) {
-            throw new NoSuchException("There is no user with ID = " + userId + " in Database");
+        if(userRepository.findById(userId).isEmpty()){
+            throw new NoSuchException("There is no user with ID = "+ userId + " in Database");
         }
-
         userRepository.deleteById(userId);
-    }
-
-    @Transactional
-    public UserDTO changeUser(Integer userId, UserDTO userDTO) {
-        Optional<User> optionalUser = userRepository.findById(userId);
-        if (optionalUser.isEmpty()) {
-            throw new NoSuchException("There is no user with ID = " + userId + " in Database");
-        } else {
-            User existingUser = optionalUser.get();
-            User updatedUser = userMapper.toEntity(userDTO);
-            existingUser.setEmail(updatedUser.getEmail());
-            existingUser.setPassword(updatedUser.getPassword());
-            existingUser.setLastname(updatedUser.getLastname());
-            existingUser.setName(updatedUser.getName());
-            existingUser.setRole(updatedUser.getRole());
-
-            return userMapper.toDto(userRepository.save(existingUser));
-        }
-    }
-
-    @Transactional
-    public UserDTO saveUser(UserDTO userDTO) {
-        log.info("Saving user: {}", userDTO);
-        User savedUser = userRepository.save(userMapper.toEntity(userDTO));
-        return userMapper.toDto(savedUser);
     }
 }
